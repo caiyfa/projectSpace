f526395779c9290bb048f4a086f08383aa0e9878
蔡云飞 <caiyfa@yonyou.com>
2020-10-30 10:20:33 +0800

    CYF

--- Tools/src/psn/cyf/test/TestMark1.java
+++ Tools/src/psn/cyf/test/TestMark1.java
@@ -2,6 +2,6 @@ package psn.cyf.test;
 
 public class TestMark1 {
     public static void main(String[] args) {
-        System.out.println("TestMark1");
+        System.out.println("TestMark1 Mark1");
     }
 }
