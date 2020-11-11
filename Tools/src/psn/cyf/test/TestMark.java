34147a6d127a22f0c7998a6964c5f66c762d82dd
CYF <18752096103@163.com>
2020-11-11 15:42:45 +0800

    2020-11-11同步 switch测试

--- Tools/src/psn/cyf/test/TestMark.java
+++ Tools/src/psn/cyf/test/TestMark.java
@@ -3,11 +3,13 @@ package psn.cyf.test;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
 
 public class TestMark {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         JSONObject jsonObject=new JSONObject();
         jsonObject.put("rowMax",5000);
         jsonObject.put("currentPage",5);
@@ -20,6 +22,8 @@ public class TestMark {
         jsonObject2.put("totalPage",25);
         jsonObject2.put("currentPage",5);
         jsonObject2.put("data",array);
+        jsonObject2.put("success",true);
+        jsonObject2.put("msg","");
         JSONObject data1=new JSONObject();
         data1.put("bankName","中国银行上海市虹桥临空经济园区支行");
         data1.put("branchBankNum","104290030166");
@@ -47,8 +51,18 @@ public class TestMark {
         JSONObject jsonObject4=new JSONObject();
         JSONArray array1=new JSONArray();
         jsonObject4.put("deletePks",array1);
+        jsonObject4.put("success",true);
+        jsonObject4.put("msg","");
         array1.put("1001SX10000000009KB9");
         array1.put("1001SX10000000009KH9");
         System.out.println(jsonObject4.toString());
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
+        LinkedHashMap<String, Object> values = new LinkedHashMap<>();
+        values.put("busidate","2020-11-08 18:10:09");
+        String busiDate=isNullWithTrim(values.get("busidate"))?sdf.format(new Date()) :sdf.format(sdf.parse(values.get("busidate").toString().trim()));
+        System.out.println(busiDate);
+    }
+    private static  boolean isNullWithTrim(Object value){
+        return value==null||value.toString().trim().length()==0;
     }
 }
---
+++ Tools/src/psn/cyf/test/TestSwitch.java
@@ -0,0 +1,10 @@
+package psn.cyf.test;
+
+public class TestSwitch {
+    public static void main(String[] args) {
+        String value="ABD";
+        String formula="select * from sm_user where user_code=#";
+        System.out.println(value.split("B").length);
+
+    }
+}
